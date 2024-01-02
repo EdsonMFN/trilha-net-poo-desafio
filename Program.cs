5c7d5cad61c4e6a928e0b72973181a790afc1e66
Edson Machado <edsonfernandes.n@outlook.com>
2024-01-02 12:25:23 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Criando Nokia");
+Smartphone n = new Nokia(numero : "81997453840", modelo : "Nokia 8", imei : "33333333", memoria : 128);
+n.Ligar();
+n.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Criando Iphone");
+Smartphone p = new Iphone(numero : "81996536533", modelo : "Iphone 8 plus", imei : "44444444", memoria : 256);
+p.ReceberLigacao();
+p.InstalarAplicativo("Kindle");
